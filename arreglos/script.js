f0088dd262ac497a53ee0e27c1e43d6f310f0669
alumno <alumno@Instituto.local>
2025-05-22 11:18:57 -0300

    r1

--- arreglos/script.js
+++ arreglos/script.js
@@ -1,3 +1,4 @@
+//ejercicio 1
 let frutas = ["naranja", "manzana", "pera"]
 frutas.push ("sandia")
 frutas.unshift ("uva")
@@ -14,6 +15,26 @@ alert(nombres.length)
 //Ejercicio 3
 let numeros =[1,2,3,4,5,6,7,8,9,10]
 for (let indice = 0; indice <= numeros.length; indice++) {
-    const element = array[indice];
     console.log(indice)
 }
+
+//ejercicio 4
+const numeros4 = [1, 2, 3, 4, 5];
+let sumaTotal = 0;
+
+for (let i = 0; i < numeros4.length; i++) {
+  sumaTotal += numeros4[i];
+}
+
+console.log(sumaTotal);
+
+//ejercicio 5
+
+let nombres1 = prompt ("Escribe 5  divididas en cada espacio")
+let nombres2 = prompt ("pon un nombre")
+let nombres3 = prompt ("pon un nombre")
+let nombres4 = prompt ("pon un nombre")
+let nombres5 = prompt ("pon un nombre")
+let registroNombres5 = []
+registroNombres5.push (nombres1,nombres2,nombres3,nombres4,nombres5)
+alert (registroNombres5)
