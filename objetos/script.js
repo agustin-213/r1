39423892b64a425e52b86eda89501004a33815dc
alumno <alumno@Instituto.local>
2025-07-31 11:17:27 -0300

    r1

--- objetos/script.js
+++ objetos/script.js
@@ -9,22 +9,39 @@
 }
 console.log (estudiante.nombre + estudiante.apellido)
 */
+
+//Cada objeto debera estar separado por una coma si no ocurre un error
 let estudiantes = [
     {
         nombre: "Jazmin",
         apellido: "Rodriguez",
-        edad:16 
-    }
+        edad:16,
+        nota:7
+    },
     {
-        nombre: "Matias",
+        nombre: "Matias",       
         apellido: "Rosello",
-        edad:16 
-    }
+        edad:16,
+        nota:7.5 
+    },
     {
         nombre: "Agustin",
         apellido: "Avila",
-        edad:16 
-    }
+        edad:16 ,
+        nota:10
+    },
+    {
+        nombre: "Jeronimo",
+        apellido: "Orfila",
+        edad:16,
+        nota:3
+    },
+    {
+        nombre: "Damaris",
+        apellido: "Villegas",
+        edad:16,
+        nota:9.89 
+    },
 ]
 
 //El corchete indica que posicion del arreglo se usara
@@ -32,6 +49,6 @@ console.log (estudiantes[1].nombre)
 
 //mostrara en la consola todos los obetos
 for (let index = 0; index < estudiantes.length; index++) {
-    console.log(estudiantes[index].apellido + "" + estudiantes[index])
+    console.log(estudiantes[index].apellido + "" + estudiantes[index] + "Su nota numerica del examen es:" + "" +  nota[index])
     
 }
