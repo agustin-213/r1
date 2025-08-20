b85c69b6354fb190b26b01a77fdbcdfdbf83efc5
alumno <alumno@Instituto.local>
2025-08-20 08:20:15 -0300

    r1

---
+++ pokedex/script.js
@@ -0,0 +1,4 @@
+// 1) Guardamos referencias a elementos del DOM por su id
+let titulo = document.querySelector('#titulo')    // <h1 id="titulo">
+let imagen = document.querySelector('#imagen')    // <img id="imagen">
+let boton = document.querySelector('#boton')      // <button id="boton">
